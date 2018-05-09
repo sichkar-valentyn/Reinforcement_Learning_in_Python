44b3b1b8ed0463c8ec7441ea03613f5e217773b4
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-05-09 16:18:57 +0300

    Add files via upload

---
+++ RL_Sarsa_E1/agent_brain.py
@@ -0,0 +1,128 @@
+# File - agent_brain.py
+# Creating brain for the agent based on the Sarsa
+
+# Importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# Importing function from the env.py
+from env import final_states
+
+# Creating class for the SarsaTable
+class SarsaTable:
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        # List of actions
+        self.actions = actions
+        # Learning rate
+        self.lr = learning_rate
+        # Value of gamma
+        self.gamma = reward_decay
+        # Value of epsilon
+        self.epsilon = e_greedy
+        # Creating full Q-table for all cells
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
+        # Creating Q-table for cells of the final route
+        self.q_table_final = pd.DataFrame(columns=self.actions, dtype=np.float64)
+
+    # Function for choosing the action for the agent
+    def choose_action(self, observation):
+        # Checking if the state exists in the table
+        self.check_state_exist(observation)
+        # Selection of the action - 90 % according to the epsilon == 0.9
+        # Choosing the best action
+        if np.random.uniform() < self.epsilon:
+            state_action = self.q_table.loc[observation, :]
+            state_action = state_action.reindex(np.random.permutation(state_action.index))
+            action = state_action.idxmax()
+        else:
+            # Choosing random action - left 10 % for choosing randomly
+            action = np.random.choice(self.actions)
+        return action
+
+    # Function for learning and updating Q-table with new knowledge
+    def learn(self, state, action, reward, next_state, next_action):
+        # Checking if the next step exists in the Q-table
+        self.check_state_exist(next_state)
+
+        # Current state in the current position
+        q_predict = self.q_table.loc[state, action]
+
+        # Checking if the next state is free or it is obstacle or goal
+        if next_state != 'goal' or next_state != 'obstacle':
+            q_target = reward + self.gamma * self.q_table.loc[next_state, next_action]
+        else:
+            q_target = reward
+
+        # Updating Q-table with new knowledge
+        self.q_table.loc[state, action] += self.lr * (q_target - q_predict)
+
+        return self.q_table.loc[state, action]
+
+    # Adding to the Q-table new states
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            self.q_table = self.q_table.append(
+                pd.Series(
+                    [0]*len(self.actions),
+                    index=self.q_table.columns,
+                    name=state,
+                )
+            )
+
+    # Printing the Q-table with states
+    def print_q_table(self):
+        # Getting the coordinates of final route from env.py
+        e = final_states()
+
+        # Comparing the indexes with coordinates and writing in the new Q-table values
+        for i in range(len(e)):
+            state = str(e[i])  # state = '[5.0, 40.0]'
+            # Going through all indexes and checking
+            for j in range(len(self.q_table.index)):
+                if self.q_table.index[j] == state:
+                    self.q_table_final.loc[state, :] = self.q_table.loc[state, :]
+
+        print()
+        print('Length of final Q-table =', len(self.q_table_final.index))
+        print('Final Q-table with values from the final route:')
+        print(self.q_table_final)
+
+        print()
+        print('Length of full Q-table =', len(self.q_table.index))
+        print('Full Q-table:')
+        print(self.q_table)
+
+    # Plotting the results for the number of steps
+    def plot_results(self, steps, cost):
+        #
+        f, (ax1, ax2) = plt.subplots(nrows=1, ncols=2)
+        #
+        ax1.plot(np.arange(len(steps)), steps, 'b')
+        ax1.set_xlabel('Episode')
+        ax1.set_ylabel('Steps')
+        ax1.set_title('Episode via steps')
+
+        #
+        ax2.plot(np.arange(len(cost)), cost, 'r')
+        ax2.set_xlabel('Episode')
+        ax2.set_ylabel('Cost')
+        ax2.set_title('Episode via cost')
+
+        plt.tight_layout()  # Function to make distance between figures
+
+        #
+        plt.figure()
+        plt.plot(np.arange(len(steps)), steps, 'b')
+        plt.title('Episode via steps')
+        plt.xlabel('Episode')
+        plt.ylabel('Steps')
+
+        #
+        plt.figure()
+        plt.plot(np.arange(len(cost)), cost, 'r')
+        plt.title('Episode via cost')
+        plt.xlabel('Episode')
+        plt.ylabel('Cost')
+
+        # Showing the plots
+        plt.show()
---
+++ RL_Sarsa_E2/run_agent.py
@@ -0,0 +1,77 @@
+# File - run_agent.py
+# Running algorithm
+
+# Importing classes
+from env import Environment
+from agent_brain import SarsaTable
+
+
+def update():
+    # Resulted list for the plotting Episodes via Steps
+    steps = []
+
+    # Summed costs for all episodes in resulted list
+    all_costs = []
+
+    for episode in range(6000):
+        # Initial Observation
+        observation = env.reset()
+
+        # Updating number of Steps for each Episode
+        i = 0
+
+        # Updating the cost for each episode
+        cost = 0
+
+        # RL choose action based on observation
+        action = RL.choose_action(str(observation))
+
+        while True:
+            # Refreshing environment
+            env.render()
+
+            # RL takes an action and get the next observation and reward
+            observation_, reward, done = env.step(action)
+
+            # RL choose action based on next observation
+            action_ = RL.choose_action(str(observation_))
+
+            # RL learns from the transition and calculating the cost
+            cost += RL.learn(str(observation), action, reward, str(observation_), action_)
+
+            # Swapping the observations and actions - current and next
+            observation = observation_
+            action = action_
+
+            # Calculating number of Steps in the current Episode
+            i += 1
+
+            # Break while loop when it is the end of current Episode
+            # When agent reached the goal or obstacle
+            if done:
+                steps += [i]
+                all_costs += [cost]
+                break
+
+    # Showing the final route
+    env.final()
+
+    # Showing the Q-table with values for each action
+    RL.print_q_table()
+
+    # Plotting the results
+    RL.plot_results(steps, all_costs)
+
+
+# Commands to be implemented after running this file
+if __name__ == "__main__":
+    # Calling for the environment
+    env = Environment()
+    # Calling for the main algorithm
+    RL = SarsaTable(actions=list(range(env.n_actions)),
+                    learning_rate=1,
+                    reward_decay=0.9,
+                    e_greedy=0.9)
+    # Running the main loop with Episodes by calling the function update()
+    env.after(100, update)  # Or just update()
+    env.mainloop()
